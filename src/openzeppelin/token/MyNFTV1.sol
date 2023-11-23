1342e5d0bd4bfbcf5f31ef193a17af95ed274eef
keesmark <keesmark@gmail.com>
2023-11-23 23:30:05 +0900

    add remappings

--- src/openzeppelin/token/MyNFTV1.sol
+++ src/openzeppelin/token/MyNFTV1.sol
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MyNFTV1 is ERC721, Ownable {
+contract MyNFTV1 is
+    Initializable,
+    ERC721Upgradeable,
+    OwnableUpgradeable,
+    UUPSUpgradeable
+{
     uint256 private _nextTokenId;
 
-    constructor(
-        address initialOwner
-    ) ERC721("MyNFT", "MN") Ownable(initialOwner) {}
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __ERC721_init("MyNFT", "MN");
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
