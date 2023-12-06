bf6aeb757e9ee10b158467bd467b8d913ed6ba1f
keesmark <keesmark@gmail.com>
2023-12-06 23:54:16 +0900

    uups upgradable

--- src/openzeppelin/token/MyNFTV2.sol
+++ src/openzeppelin/token/MyNFTV2.sol
@@ -5,11 +5,38 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {MyNFTV1} from "./MyNFTV1.sol";
+import {ERC1967Utils} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
 
-contract MyNFTV2 is MyNFTV1 {
+contract MyNFTV2 is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 private _nextTokenId;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __ERC721_init("MyNFT", "MN");
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) public onlyOwner {
+        _update(address(0), tokenId, address(0));
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function getImplementation() external view returns (address) {
+        return ERC1967Utils.getImplementation();
+    }
+
+    function getVersion() external pure returns (string memory) {
+        return "2";
     }
 }
--- test/openzeppelin/token/MyNFTV1.t.sol
+++ test/openzeppelin/token/MyNFTV1.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {MyNFTV1} from "../../../src/openzeppelin/token/MyNFTV1.sol";
 import {MyNFTV2} from "../../../src/openzeppelin/token/MyNFTV2.sol";
+import {UUPSProxy} from "../../../src/openzeppelin/proxy/UUPSProxy.sol";
 import "forge-std/console.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
@@ -11,13 +12,13 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeab
 contract MyNFTTest is Test {
     MyNFTV1 myNFTV1;
     address owner;
-    address proxy;
+    UUPSProxy uupsProxy;
 
     function setUp() public {
         owner = vm.addr(1);
         bytes memory initData = abi.encodeCall(MyNFTV1.initialize, owner);
-        proxy = address(new ERC1967Proxy(address(new MyNFTV1()), initData));
-        myNFTV1 = MyNFTV1(proxy);
+        uupsProxy = new UUPSProxy(address(new MyNFTV1()), initData);
+        myNFTV1 = MyNFTV1(address(uupsProxy));
     }
 
     function test_SafeMint() public {
@@ -46,20 +47,19 @@ contract MyNFTTest is Test {
     }
 
     function test_upgrade() public {
-        vm.prank(owner);
+        vm.startPrank(owner);
         address other = vm.addr(2);
         myNFTV1.safeMint(other);
         assertEq(myNFTV1.balanceOf(other), 1);
 
-        // myNFTV1.upgradeToAndCall(address(new MyNFTV2()), "");
-        // assertEq(myNFTV2.balanceOf(other), 1);
-
-        // assertEq(owner, myNFTV2.owner());
-        // bytes memory initData = abi.encodeCall(MyNFTV2., owner);
-        // UUPSUpgradeable(proxy).upgradeToAndCall(address(new MyNFTV2()), "");
         myNFTV1.upgradeToAndCall(address(new MyNFTV2()), "");
+
+        address proxy = address(uupsProxy);
         MyNFTV2 myNFTV2 = MyNFTV2(proxy);
-        // myNFTV2.upgradeToAndCall(address(myNFTV1), "");
+
+        assertEq(myNFTV2.balanceOf(other), 1);
+        assertEq(owner, myNFTV2.owner());
+
         myNFTV2.safeMint(other);
         assertEq(myNFTV2.balanceOf(other), 2);
     }
